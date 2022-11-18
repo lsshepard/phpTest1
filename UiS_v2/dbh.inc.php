45013fc2637b5efaa788571d1b66a88a6dfe27e4
lsshepard <100352314+lsshepard@users.noreply.github.com>
2022-11-18 20:07:57 +0100

    Update dbh.inc.php

--- UiS_v2/dbh.inc.php
+++ UiS_v2/dbh.inc.php
@@ -1,9 +1,9 @@
 <?php
 
-$serverName = "kczasktwxv";
-$dBUsername = "kczasktwxv";
-$dBPassword = "T4r4smWxq9";
-$dBName = "uis1";  
+$serverName = "";
+$dBUsername = "";
+$dBPassword = "";
+$dBName = "";  
 
 $conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
 
